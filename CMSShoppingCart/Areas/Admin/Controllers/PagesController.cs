1deb8c5c1ef6a5ddb5d24631863d35c9d9736be1
legil <leslieg033@gmail.com>
2020-02-24 22:35:37 -0500

    adding barebones 'area' for admin

---
+++ CMSShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CMSShoppingCart.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMSShoppingCart.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class PagesController : Controller
+    {
+        private readonly CMSShoppingCartContext context;
+
+        public PagesController(CMSShoppingCartContext context)
+        {
+            this.context = context;
+        }
+        public string Index()
+        {
+            return "test";
+        }
+    }
+}
--- CMSShoppingCart/Startup.cs
+++ CMSShoppingCart/Startup.cs
@@ -49,11 +49,20 @@ namespace CMSShoppingCart
 
             app.UseAuthorization();
 
+
+
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+          );
+
+
+                endpoints.MapControllerRoute(
+                        name: "default",
+                        pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
